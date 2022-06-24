91701ca323a91cc9e91fbc633e38b308d28693b8
Logan Deal <logan.deal27@gmail.com>
2022-06-24 11:21:46 -0500

    Add database functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,30 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from peewee import *
+import datetime as dt
+from playhouse.shortcuts import model_to_dict
+
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+
+print(mydb)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=dt.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 load_dotenv()
 app = Flask(__name__)
@@ -36,6 +60,25 @@ def royhobby():
 @app.route('/aboutus/')
 def aboutus():
     return render_template('aboutus.html')
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in
+TimelinePost.select().order_by(TimelinePost.created_at.desk())
+        ]
+    }
 
 if __name__ ==  "__main__":
     app.run()
