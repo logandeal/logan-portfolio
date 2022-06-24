dd09fd027cebf524db692c0320e486fabe407e9f
Logan Deal <logan.deal27@gmail.com>
2022-06-24 16:25:18 +0000

    Add chmod in redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -13,3 +13,5 @@ tmux new -s portfolio -d
 tmux send-keys -t portfolio "cd logan-portfolio" Enter
 tmux send-keys -t portfolio "source python3-virtualenv/bin/activate" Enter
 tmux send-keys -t portfolio "flask run --host=0.0.0.0" Enter
+
+chmod +x redeploy-site.sh
