2cb05bb22469eed39916711bdefe827d032b8777
Logan Deal <logan.deal27@gmail.com>
2022-06-27 01:07:25 +0000

    change redeployment script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-tmux kill-server
-
 cd logan-portfolio
 
 git fetch && git reset origin/main --hard
@@ -9,9 +7,7 @@ git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip3 install -r requirements.txt
 
-tmux new -s portfolio -d
-tmux send-keys -t portfolio "cd logan-portfolio" Enter
-tmux send-keys -t portfolio "source python3-virtualenv/bin/activate" Enter
-tmux send-keys -t portfolio "flask run --host=0.0.0.0" Enter
+systemctl daemon-reload
+systemctl restart myportfolio
 
 chmod +x redeploy-site.sh
