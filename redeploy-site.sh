55ac0f1acb9b60821372b0a843dbd7fdfadcc24c
Logan Deal <53441579+logandeal@users.noreply.github.com>
2022-07-13 18:29:47 -0500

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -5,6 +5,6 @@ cd logan-portfolio
 git fetch && git reset origin/main --hard
 
 docker compose -f docker-compose.prod.yml down
-docker compose -f docker-compose.prod.yml -d --build
+docker compose -f docker-compose.prod.yml up -d --build
 
 chmod +x redeploy-site.sh
